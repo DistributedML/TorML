Fixing up magic dataset, normalization and polybasis

ML/code/logistic_main.py
@@ -10,7 +10,7 @@ import pdb
 
 if __name__ == "__main__":
 
-    dataset = "susy4"
+    dataset = "magic1"
     data = utils.load_dataset(dataset)
 
     print("Download complete.")
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     y = data['y']
 
     batch_size = 50
-    iterations = 20000
+    iterations = 100000
 
     # Global
     numFeatures = logistic_model.init(dataset)
@@ -28,15 +28,20 @@ if __name__ == "__main__":
     rolling_average = np.zeros([iterations, numFeatures])
     all_delta = np.zeros([iterations, numFeatures])
     progress = np.zeros(iterations)
+    train_progress = np.zeros(iterations)
+    test_progress = np.zeros(iterations)
 
     for i in xrange(iterations):
         (ff, gg, deltas) = logistic_model.privateFun(1, weights, batch_size)
         weights = weights + deltas
         progress[i] = ff
+        train_progress[i] = logistic_model_test.train_error(weights)
+        test_progress[i] = logistic_model_test.test_error(weights)
         if i % 1000 == 0:
             print(i)
 
-    plt.plot(progress)
+    plt.plot(train_progress, "green")
+    plt.plot(test_progress, "red")
 
     #pdb.set_trace()
 

ML/code/utils.py
@@ -120,7 +120,7 @@ def load_dataset(dataset_name):
 
     elif dataset_name == "magic":
 
-        magic = pd.read_csv(os.path.join('../ML', "data", 'magic04.data'))
+        magic = pd.read_csv(os.path.join('..', "data", 'magic04.data'))
         nn, dd = magic.shape
 
         y = magic.ix[:, dd - 1].as_matrix()
@@ -128,12 +128,17 @@ def load_dataset(dataset_name):
         y[np.where(y == 'h')] = -1
 
         npmagic = magic.ix[np.random.permutation(
-            nn), :].as_matrix().astype(int)
+            nn), :].as_matrix().astype(float)
         split = int(nn * 0.70)
 
-        X = npmagic[0:split - 1, 0:dd - 2]
-        y = npmagic[0:split - 1, dd - 1]
-        Xvalid = npmagic[split:nn - 1, 0:dd - 2]
+        # Add another column for the square of each value
+        data = npmagic[:, 0:dd-1]
+        data = np.hstack([data, (data * data)])
+
+        X = data[0:split, :]
+        y = npmagic[0:split, dd - 1]
+
+        Xvalid = data[split:nn - 1]
         yvalid = npmagic[split:nn - 1, dd - 1]
 
         X, mu, sigma = standardize_cols(X)
@@ -179,7 +184,7 @@ def load_dataset(dataset_name):
 
         # This is the main section typically called by the tor client.
         # Thus we have hardcoded an absolute path from that location
-        data = pd.read_csv(os.path.join('../ML', "data", dataset_name + '.csv'))
+        data = pd.read_csv(os.path.join('..', "data", dataset_name + '.csv'))
         d = data.shape[1]
 
         data = data.as_matrix()
@@ -191,11 +196,16 @@ def load_dataset(dataset_name):
 
 def normalize_rows(X):
 
-    # Sets all rows to have L2 norm of 1. Needed for diff priv
+    # Sets the max row to have L2 norm of 1. Needed for diff priv
     nn, dd = X.shape
+    max_norm = 0
 
     for i in xrange(nn):
-        X[i, ] = X[i, ] / norm(X[i, ], 2)
+        new_norm = norm(X[i, ], 2)
+        if new_norm > max_norm:
+            max_norm = new_norm
+
+    X = X / max_norm
 
     return X
 