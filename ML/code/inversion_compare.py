4c1d297c6ddb1ae5b15843829dc503627afa0709
clementfung <clement.y.fung@gmail.com>
2017-10-22 16:17:22 -0700

    Fixing inversion comparison code

--- ML/code/inversion_compare.py
+++ ML/code/inversion_compare.py
@@ -1,8 +1,13 @@
 from sklearn.linear_model import SGDClassifier
 from sklearn.svm import LinearSVC
 import numpy as np
+import sys
+import utils
+import pdb
 
-if __name__ == "main":
+if __name__ == "__main__":
+
+	victim_csv = sys.argv[1]
 
 	# Inverted attacked dataset
 	data = utils.load_dataset("credit1")
@@ -15,9 +20,14 @@ if __name__ == "main":
 	clf = SGDClassifier(loss="hinge", penalty="l2")
 	clf.fit(X, y)
 
-	clf.predict(Xvalid)
+	real_hat = clf.predict(Xvalid)
 
-	
+	victim_data = np.loadtxt(victim_csv, delimiter=',')
+	victim_model = victim_data[0,:]
 
+	victim_model = np.array(victim_model)
+	victim_hat = np.sign(np.dot(Xvalid, victim_model))
+
+	print("Agreement is %.3f" % (sum(victim_hat == real_hat) / float(real_hat.shape[0])))
 
 
