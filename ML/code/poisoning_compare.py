f874bb6b3b6f49a2f2eab1ae17c8b895af1a77e2
clementfung <clement.y.fung@gmail.com>
2018-04-28 19:15:29 -0700

    Compare poisoning attack strength

--- ML/code/poisoning_compare.py
+++ ML/code/poisoning_compare.py
@@ -8,37 +8,59 @@ def main():
     dataTrain = np.load("../data/mnist_train.npy")
     dataTest = np.load("../data/mnist_train.npy")
 
-    df1 = pd.read_csv("../../DistSys/modelflush_1p.csv", header=None)
-    poison_model = df1.ix[0, :7839].as_matrix().astype(float)
+    df = pd.read_csv("../../DistSys/modelflush_pure.csv", header=None)
+    pure_model = df.ix[0, :7839].as_matrix().astype(float)
 
-    df2 = pd.read_csv("../../DistSys/modelflush_pure.csv", header=None)
-    pure_model = df2.ix[0, :7839].as_matrix().astype(float)
-
-    Xtrain = dataTrain[:, :784]
-    ytrain = dataTrain[:, 784]
+    Xtrain = dataTest[:, :784]
+    ytrain = dataTest[:, 784]
 
     # hardcoded for MNIST
     W = np.reshape(pure_model, (10, 784))
     yhat = np.argmax(np.dot(Xtrain, W.T), axis=1)
 
     targetIdx = np.where(ytrain == 1)
+    otherIdx = np.where(ytrain != 1)
+    overall = np.mean(yhat[otherIdx] == ytrain[otherIdx])
     correct1 = np.mean(yhat[targetIdx] == 1)
     attacked1 = np.mean(yhat[targetIdx] == 7)
 
-    print("Target Training Error on 1s: " + str(correct1))
-    print("Target Attack Rate (1 to 7): " + str(attacked1))
+    print("Overall Error: " + str(overall))
+    print("Target Training Accuracy on 1s: " + str(correct1))
+    print("Target Attack Rate (1 to 7): " + str(attacked1) + "\n")
+
+    df = pd.read_csv("../../DistSys/modelflush_1p.csv", header=None)
+    poison_model = df.ix[0, :7839].as_matrix().astype(float)
 
     # hardcoded for MNIST
     W = np.reshape(poison_model, (10, 784))
     yhat = np.argmax(np.dot(Xtrain, W.T), axis=1)
 
     targetIdx = np.where(ytrain == 1)
+    otherIdx = np.where(ytrain != 1)
+    overall = np.mean(yhat[otherIdx] == ytrain[otherIdx])
     correct1 = np.mean(yhat[targetIdx] == 1)
     attacked1 = np.mean(yhat[targetIdx] == 7)
 
-    print("Target Training Error on 1s: " + str(correct1))
-    print("Target Attack Rate (1 to 7): " + str(attacked1))
+    print("Overall Error: " + str(overall))
+    print("Target Training Accuracy on 1s: " + str(correct1))
+    print("Target Attack Rate (1 to 7): " + str(attacked1) + "\n")
 
+    df = pd.read_csv("../../DistSys/modelflush_2p.csv", header=None)
+    poison_model = df.ix[0, :7839].as_matrix().astype(float)
+
+    # hardcoded for MNIST
+    W = np.reshape(poison_model, (10, 784))
+    yhat = np.argmax(np.dot(Xtrain, W.T), axis=1)
+
+    targetIdx = np.where(ytrain == 1)
+    otherIdx = np.where(ytrain != 1)
+    overall = np.mean(yhat[otherIdx] == ytrain[otherIdx])
+    correct1 = np.mean(yhat[targetIdx] == 1)
+    attacked1 = np.mean(yhat[targetIdx] == 7)
+
+    print("Overall Error: " + str(overall))
+    print("Target Training Accuracy on 1s: " + str(correct1))
+    print("Target Attack Rate (1 to 7): " + str(attacked1) + "\n")
 
     pdb.set_trace()
 
