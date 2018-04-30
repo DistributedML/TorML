eea8a8dd846ccecc3ad2b02cc1a9353601274d13
Chris Yoon <chris.jm.yoon@gmail.com>
2018-04-30 12:48:24 -0700

    Add misslabel script to new branch

---
+++ ML/code/misslabel_mnist.py
@@ -0,0 +1,26 @@
+import numpy as np
+import sys
+import os
+
+''' Mislabels preprocessed mnist data
+Takes input
+    - MNIST dataset. eg 4
+    - misslabel : eg 9
+ and saves updated dataset as "bad_mnist4_9.npy"
+'''
+
+def main(argv):
+    print(argv)
+    dataset = "mnist"
+    dataset +=  argv[0]
+    print(dataset)
+    data = np.load(os.path.join('../ML', "data", dataset + '.npy'))
+    data[:, -1] += int(argv[1]) - int(argv[0])
+    save_file = "mnist_bad_" + argv[0] + argv[1]
+    np.save(save_file, data)
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
