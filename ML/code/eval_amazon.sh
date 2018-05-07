3a23e8a0152ca717af88b56c08927d7b77ddcfc4
Chris Yoon <chris.jm.yoon@gmail.com>
2018-05-06 20:21:53 -0700

    Pushign to lev

--- ML/code/eval_amazon.sh
+++ ML/code/eval_amazon.sh
@@ -14,4 +14,4 @@ python code/ML_main.py amazon 100 5_0_10 5_5_25 5_20_40 5_35_10 5_20_15
 python code/ML_main.py amazon 100 5_0_10 5_5_25
 
 #0_11, 2_11, 3_11, 5_11, 13_11, 15_11
-python code/ML_main.py amazon 100 5_0_40 5_5_40 5_10_40 5_20_40 5_25_40
+python code/ML_main.py amazon 5 5_0_40 5_5_40 5_10_40 5_20_40 5_25_40
