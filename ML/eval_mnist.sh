fb63c8a04ff4f59ffd1911a84d918c586ccedac6
Chris Yoon <chris.jm.yoon@gmail.com>
2018-05-06 20:42:37 -0700

    Moved eval files out to ML

--- ML/eval_mnist.sh
+++ ML/eval_mnist.sh
@@ -1,5 +1,5 @@
 # Single poisoners (We could also change eval_grid from 5 to 1)
-python code/ML_main.py mnist 3000 1_1_7
+python code/ML_main.py mnist 3000 1_1_7 
 python code/ML_main.py mnist 3000 1_4_9
 python code/ML_main.py mnist 3000 1_0_8
 
