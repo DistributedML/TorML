2c06ad4c0dc7f4b875c2afc108c214b83ed5a30c
clementfung <clement.y.fung@gmail.com>
2018-05-30 13:09:27 -0700

    basic results

--- DistSys/localscript.sh
+++ DistSys/localscript.sh
@@ -1,7 +1,16 @@
-go run torcurator.go c1 5 l &
+#!/bin/sh
+
+export GOPATH=/home/cfung/gospace
+
+go run torcurator.go c1 10 l &
 wait
 go run torclient.go h1 credit/credit1 1 l &
 go run torclient.go h2 credit/credit2 1 l &
 go run torclient.go h3 credit/credit3 1 l &
 go run torclient.go h4 credit/credit4 1 l &
 go run torclient.go h5 credit/credit5 1 l &
+go run torclient.go h6 credit/credit1 1 l &
+go run torclient.go h7 credit/credit2 1 l &
+go run torclient.go h8 credit/credit3 1 l &
+go run torclient.go h9 credit/credit4 1 l &
+go run torclient.go h10 credit/credit5 1 l &
