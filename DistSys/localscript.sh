636cc14c2ccf62516825164db7043d3be8e68e4c
clementfung <clement.y.fung@gmail.com>
2018-05-30 17:31:26 -0700

    Trim new curator delay

--- DistSys/localscript.sh
+++ DistSys/localscript.sh
@@ -2,7 +2,7 @@
 
 export GOPATH=/home/cfung/gospace
 
-go run torcurator.go c1 10 l &
+go run torcurator.go c1 8 l &
 wait
 go run torclient.go h1 credit/credit1 1 l &
 go run torclient.go h2 credit/credit2 1 l &
@@ -10,7 +10,6 @@ go run torclient.go h3 credit/credit3 1 l &
 go run torclient.go h4 credit/credit4 1 l &
 go run torclient.go h5 credit/credit5 1 l &
 go run torclient.go h6 credit/credit1 1 l &
-go run torclient.go h7 credit/credit2 1 l &
-go run torclient.go h8 credit/credit3 1 l &
-go run torclient.go h9 credit/credit4 1 l &
-go run torclient.go h10 credit/credit5 1 l &
+go run torclient.go h7 credit/creditbad 1 l &
+go run torclient.go h8 credit/creditbad 1 l &
+
