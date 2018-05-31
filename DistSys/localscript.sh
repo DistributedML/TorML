0c5a4b3bb4910dab83d42cc227081d430f8a7efa
clementfung <clement.y.fung@gmail.com>
2018-05-30 19:14:54 -0700

    add 50/50 deployment

--- DistSys/localscript.sh
+++ DistSys/localscript.sh
@@ -8,8 +8,8 @@ go run torclient.go h1 credit/credit1 1 l &
 go run torclient.go h2 credit/credit2 1 l &
 go run torclient.go h3 credit/credit3 1 l &
 go run torclient.go h4 credit/credit4 1 l &
-go run torclient.go h5 credit/credit5 1 l &
-go run torclient.go h6 credit/credit1 1 l &
+go run torclient.go h5 credit/creditbad 1 l &
+go run torclient.go h6 credit/creditbad 1 l &
 go run torclient.go h7 credit/creditbad 1 l &
 go run torclient.go h8 credit/creditbad 1 l &
 
